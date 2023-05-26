102d946a99b3710235db510072e042f0c4cb28a5
Rita2024 <ritaejezie2@gmail.com>
2023-05-26 08:26:39 -0700

    change directory

---
+++ own_cd.c
@@ -0,0 +1,25 @@
+#include "simple_shell.h"
+
+/**
+ * own_cd - change directory
+ * Return: result
+ */
+
+int own_cd(char **args)
+{
+	if (strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] == NULL)
+		{
+			fprintf(stderr, "expected argument to \"cd\"\n");
+		}
+		else
+		{
+			if (chdir(args[1]) != 0)
+			{
+				perror("error");
+			}
+		}
+		return (1);
+	}
+}
